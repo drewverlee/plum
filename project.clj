eb66c40202a44da8c20d504f284440333861ab38
drew verlee <drew verlee@gmail.com>
2017-10-28 17:29:39 -0400

    Bump clojure version to latest and code coverage tool

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
 (defproject plum "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Sorts records"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :dependencies [[org.clojure/clojure "1.9.0-alpha16"]]
   :main ^:skip-aot plum.core
   :target-path "target/%s"
+  :plugins [[lein-cloverage "1.0.9"]]
   :profiles {:uberjar {:aot :all}})
