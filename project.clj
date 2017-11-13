da74a3dfbc30efae1e90e79aaca4b589d50abfa2
drew verlee <drew.verlee@gmail.com>
2017-11-13 03:20:08 -0500

    Bump clojure to lateset version.

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description "Sorts records"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0-alpha16"]]
+  :dependencies [[org.clojure/clojure "1.9.0-alpha19"]]
   :main ^:skip-aot plum.core
   :target-path "target/%s"
   :plugins [[lein-cloverage "1.0.9"]]
