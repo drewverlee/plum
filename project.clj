bb590ac1f4b3ec2820e809d43d652ab94112881d
drew verlee <drew.verlee@gmail.com>
2017-11-13 04:07:35 -0500

    Add msgs for command line arguments and a way to generate user friendly error messages.

--- project.clj
+++ project.clj
@@ -2,7 +2,8 @@
   :description "Sorts records"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0-alpha19"]]
+  :dependencies [[org.clojure/clojure "1.9.0-alpha19"]
+                 [com.gfredericks/like-format-but-with-named-args "0.1.3"]]
   :main ^:skip-aot plum.core
   :target-path "target/%s"
   :plugins [[lein-cloverage "1.0.9"]]
--- src/plum/command_line.clj
+++ src/plum/command_line.clj
@@ -1,6 +1,8 @@
 (ns plum.command-line
   (:require [clojure.spec.alpha :as s]
-            [clojure.java.io :as io]))
+            [clojure.java.io :as io]
+            [clojure.string :as str]
+            [com.gfredericks.like-format-but-with-named-args :refer [named-format]]))
 
 (def cli-fn-names #{"sort"})
 (def sort-fn-names #{"last-name" "birth-date" "gender-and-lastname"})
@@ -17,3 +19,58 @@
 
 (s/def ::cli-fns #(get cli-fn-names (first %)))
 (s/def ::cli-input (s/and ::cli-fns (s/or :sort ::sort)))
+
+(def cli-fn-msg "The base function")
+(def sort-csv-msg "The sort function to apply to the input csv")
+(def input-csv-msg "The existing csv location")
+(def output-csv-msg "The existing csv output location")
+
+(defn get-problem-map-leaf
+  "
+  Returns important information from the specs problem map
+  |:--
+  | `spec`| a registered clojure.core spec.
+  | `x`   | the value the spec will be called on.
+  "
+  [spec x]
+  (let [problem (first (:clojure.spec.alpha/problems (s/explain-data spec x)))]
+    {:spec (last (:via problem))
+     :val  (:val problem)
+     :pos  (first (:in problem))
+     :reason (:reason problem)
+     :path (:path problem)}))
+
+(defmulti user-friendly-msg (fn [ctx spec x m] [ctx spec]))
+
+(defmethod user-friendly-msg [:cli ::cli-fns]
+  [ctx spec x m]
+  (let [{val :val} (get-problem-map-leaf spec x)]
+    (named-format "%msg~s {%val~s} in position 0 isn't in the list of accepted functions: %cli-fns~s" {:msg cli-fn-msg
+                                                                                                       :val (first val)
+                                                                                                       :cli-fns (str/join "," cli-fn-names)})))
+
+(defmethod user-friendly-msg [:cli ::sort-fn]
+  [ctx spec x m]
+  (named-format "%msg~s {%val~s} in position {%pos~s} isn't in the list of accepted functions: %sort-fns~s"
+                (merge (get-problem-map-leaf spec x) {:msg sort-csv-msg :sort-fns (str/join "," sort-fn-names)} m)))
+
+(defmethod user-friendly-msg [:cli ::input-csv]
+  [ctx spec x m]
+  (named-format "%msg~s {%val~s} in position {%pos~s} can't be read! "
+                (merge (get-problem-map-leaf spec x) {:msg input-csv-msg} m)))
+
+(defmethod user-friendly-msg [:cli ::output-csv]
+  [ctx spec x m]
+  (named-format "%msg~s {%val~s} in position {%pos~s} can't be written to!"
+                (merge (get-problem-map-leaf spec x) {:msg output-csv-msg} m)))
+
+(defmethod user-friendly-msg [:cli nil]
+  [ctx spec x m]
+  "success")
+
+(defmethod user-friendly-msg [:cli ::cli-input]
+  [ctx spec x m]
+  (let [{reason :reason path :path val :val leaf-spec :spec pos :pos} (get-problem-map-leaf spec x)]
+    (if reason
+      (named-format "%arg~s failed because %reason~s" {:arg (name (last path)) :reason reason})
+      (user-friendly-msg ctx leaf-spec val {:pos pos}))))
