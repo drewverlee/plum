731637567cf0e43a92f0757c1d8a41eb3229b6a5
drew verlee <drew verlee@gmail.com>
2017-11-25 17:25:05 -0500

    Fix usage of clojure.test is macro

--- test/plum/command_line_test.clj
+++ test/plum/command_line_test.clj
@@ -12,16 +12,13 @@
     (with-files [[input-csv "a,b"]
                  [output-csv ""]]
       (testing "when missing arguments the returned map contains the exit action"
-        (is (= :exit
-               (:action (cli/args->action ["sort"]))
-               (:action (cli/args->action ["sort" "last-name" input-csv-path])))))
+        (is (= :exit (:action (cli/args->action ["sort"]))))
+        (is (= :exit (:action (cli/args->action ["sort" "last-name" input-csv-path])))))
       (testing "when given bad arguments the returned map contains the exit action"
-        (is (= :exit
-               (:action (cli/args->action ["bad-arg"]))
-               (:action (cli/args->action ["sort" "last-name" input-csv-path "file-that-doesnt-exist"])))))
+        (is (= :exit (:action (cli/args->action ["bad-arg"]))))
+        (is (= :exit (:action (cli/args->action ["sort" "last-name" input-csv-path "file-that-doesnt-exist"])))))
       (testing "when given the help option we exit"
-        (is (= :exit
-               (:action (cli/args->action ["--help"])))))
+        (is (= :exit (:action (cli/args->action ["--help"])))))
       (testing "When given good arguments the returned map doesn't contain the exit action"
-        (is (= :sort
-               (:action (cli/args->action ["sort" "last-name" input-csv-path output-csv-path]))))))))
+        (is (= :sort (:action (cli/args->action ["sort" "last-name" input-csv-path output-csv-path]))))))))
+
