83438a33ca7c87bed88be62f93ab6db6c1f479b5
drew verlee <drew verlee@gmail.com>
2017-11-25 16:07:45 -0500

    Add functions and specs useful for dealing with a person record, which will find in the csvs we have to sort

--- project.clj
+++ project.clj
@@ -3,8 +3,10 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0-alpha19"]
+                 [org.clojure/test.check "0.10.0-alpha2"]
                  [com.gfredericks/like-format-but-with-named-args "0.1.3"]
-                 [org.clojure/tools.cli "0.3.5"]]
+                 [org.clojure/tools.cli "0.3.5"]
+                 [clj-time "0.14.0"]]
   :main ^:skip-aot plum.core
   :target-path "target/%s"
   :plugins [[lein-cloverage "1.0.9"]]
---
+++ src/plum/person.clj
@@ -0,0 +1,44 @@
+(ns plum.person
+  (:require [clj-time.format :as f]
+            [clj-time.coerce :as c]
+            [clj-time.core :as t]
+            [clojure.spec.alpha :as s]
+            [clojure.spec.gen.alpha :as gen]
+            [clojure.string :as str]))
+
+(def date-of-birth-formatter (f/formatter "MM/dd/yyyy"))
+
+(defn date-time->date-of-birth
+  [dt]
+  (f/unparse date-of-birth-formatter  dt))
+
+(def date-gen
+  (gen/fmap  #(f/unparse  (c/from-date %))
+             (s/gen (s/inst-in #inst "1920" #inst "1985"))))
+
+(s/def ::date-of-birth
+  (s/with-gen #(string? %)
+    (constantly date-gen)))
+
+(def base-spec
+  (s/with-gen (s/and #(not (#{"," "|" " "} %))  #(> (count %) 1))
+    #(s/gen string?)))
+
+(s/def ::last-name base-spec)
+(s/def ::first-name base-spec)
+(s/def ::gender base-spec)
+(s/def ::favorite-color base-spec)
+
+(s/def ::entity (s/cat :last-name ::last-name
+                       :first-name ::first-name
+                       :gender ::gender
+                       :favorite-color ::favorite-color
+                       :date-of-birth ::date-of-birth))
+
+(defn ->vals-as-str
+  [delimeter person]
+  (as-> person p
+    (cond-> p
+      (contains? p :date-of-birth) (update :date-of-birth #(f/unparse date-of-birth-formatter  %)))
+    (vals p)
+    (str/join delimeter p)))
