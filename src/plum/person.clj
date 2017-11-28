799748cdd97946e0a907372ae8d71d30c78a4045
drew verlee <drew verlee@gmail.com>
2017-11-28 17:32:09 -0500

    Add ability to transform from dob to date time. move separators to person because its simplier. Use seprator when writing csv.

--- src/plum/person.clj
+++ src/plum/person.clj
@@ -8,6 +8,7 @@
             [clojure.spec.alpha :as s]
             [clojure.spec.gen.alpha :as gen]
             [clojure.string :as str]))
+            
 
 (def date-of-birth-formatter (f/formatter "MM/dd/yyyy"))
 
@@ -15,16 +16,27 @@
   [dt]
   (f/unparse date-of-birth-formatter  dt))
 
+(defn date-of-birth->date-time
+  [dob]
+  (f/parse date-of-birth-formatter dob))
+
 (def date-gen
   (gen/fmap  #(f/unparse  (c/from-date %))
-             (s/gen (s/inst-in #inst "1920" #inst "1985"))))
+             (s/gen (s/inst-in #inst "1800" #inst "2020"))))
 
 (s/def ::date-of-birth
   (s/with-gen string?
     (constantly date-gen)))
 
+(def separators #{"," "|" " "})
+(def separator (s/gen separators))
+
+(s/def ::separator
+  (s/with-gen string?
+    (constantly separator)))
+
 (def base-spec
-  (s/with-gen (s/and #(not (#{"," "|" " "} %))  #(> (count %) 1))
+  (s/with-gen (s/and #(not (separator %))  #(> (count %) 1))
     #(s/gen string?)))
 
 (s/def ::last-name base-spec)
@@ -38,21 +50,28 @@
                        :favorite-color ::favorite-color
                        :date-of-birth ::date-of-birth))
 
+(def record
+  (gen/fmap #(str/join (first %) (rest %))
+            (s/gen (s/cat :separator ::separator :person ::entity))))
+
+(s/def ::record
+  (s/with-gen string?
+    (constantly record)))
+
 (defn ->vals-as-str
-  [delimeter person]
+  [separator person]
   (as-> person p
     (cond-> p
       (contains? p :date-of-birth) (update :date-of-birth #(f/unparse date-of-birth-formatter %)))
     (vals p)
-    (str/join delimeter p)))
+    (str/join separator p)))
 
 (def attributes (take-nth 2 (rest (s/form ::entity))))
 
 (defn ->csv
-  [delimiter output-file persons]
+  [separator output-file persons]
   (with-open [writer (io/writer output-file)]
-    (->> persons
-         (sc/cast-with {:date-of-birth #(f/unparse date-of-birth-formatter %)})
-         sc/vectorize
-         (cd-csv/write-csv writer))))
-
+    (as-> persons p
+         (sc/cast-with {:date-of-birth date-time->date-of-birth} p)
+         (sc/vectorize p)
+         (cd-csv/write-csv writer p :separator separator))))
