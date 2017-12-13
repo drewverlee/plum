284d423116942e864bb1f8ba9353a72b8bbf8252
drew verlee <drew.verlee@gmail.com>
2017-12-12 20:32:02 -0500

    Some white space cleanup.

--- test/plum/person/core_test.clj
+++ test/plum/person/core_test.clj
@@ -2,14 +2,7 @@
   (:require [plum.person.core :as c]
             [clojure.test :refer :all]))
 
-
-
 (deftest get-separator
   (testing "Given a string one of the Separators (see accepted list) get returns a separator character"
     (is (= \| (c/get-separator "hello|you")))
     (is (= \space (c/get-separator "hello you|there")) "picks the first separator")))
-
-
-
-
-
