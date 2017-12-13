e295e1893018e86a0479c15c1acf36f904a540db
drew verlee <drew.verlee@gmail.com>
2017-12-12 20:26:58 -0500

    Add test for get separator.

---
+++ test/plum/person/core_test.clj
@@ -0,0 +1,15 @@
+(ns plum.person.core-test
+  (:require [plum.person.core :as c]
+            [clojure.test :refer :all]))
+
+
+
+(deftest get-separator
+  (testing "Given a string one of the Separators (see accepted list) get returns a separator character"
+    (is (= \| (c/get-separator "hello|you")))
+    (is (= \space (c/get-separator "hello you|there")) "picks the first separator")))
+
+
+
+
+
